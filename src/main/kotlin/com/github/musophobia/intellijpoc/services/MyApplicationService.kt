f2ca4e36a8b3b97261bba7bb5c8a07294c193f09
GitHub Action <action@github.com>
2021-08-04 06:49:19 +0000

    Template cleanup

--- src/main/kotlin/com/github/musophobia/intellijpoc/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/musophobia/intellijpoc/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.musophobia.intellijpoc.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.musophobia.intellijpoc.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/musophobia/intellijpoc/services/MyProjectService.kt
+++ src/main/kotlin/com/github/musophobia/intellijpoc/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.musophobia.intellijpoc.services
 
+import com.github.musophobia.intellijpoc.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
